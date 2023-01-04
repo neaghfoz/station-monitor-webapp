ef310fcbb5dad75556817fa3e56404874cc3683a
ljisoo <9378433+ljisoo@user.noreply.gitee.com>
2023-01-04 21:49:19 +0800

    修改標題和圖標

--- src/settings/settings-sx.js
+++ src/settings/settings-sx.js
@@ -1,10 +1,10 @@
 module.exports = {
   //山西
-  clearVersion: '1401.2208.1',
+  clearVersion: '2023.1.1001',
   logIndexTitle: '山西站级业务管理系统',
-  title: '山西站级业务管理系统',
+  title: '联网收费集中监控系统',
   logo: {type: 'img', /* img*/ src: '/img/logo_sx.png'},
-  company: '山西交控数字交通科技有限公司',
+  company: '山西交控',
   hgCompany: '',
   //password: {reg: /^(?![a-zA-Z]+$)(?![A-Z0-9]+$)(?![A-Z\W_!@#$%^&*`~()-+=]+$)(?![a-z0-9]+$)(?![a-z\W_!@#$%^&*`~()-+=]+$)(?![0-9\W_!@#$%^&*`~()-+=]+$)[a-zA-Z0-9\W_!@#$%^&*`~()-+=]{8,15}$/, msg: '8-20位大小写字母数字特殊字符组合'},
   password: {reg: /^[0-9]{6,20}$/,msg: '6-20位数字组合'},
